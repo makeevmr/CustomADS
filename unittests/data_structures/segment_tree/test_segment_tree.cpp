726e0e080451b499d7be4646093eb65955589da0
makeevmr <art.makeev2016@yandex.ru>
2025-09-26 08:50:12 +0300

    test: add tests for segment tree

--- unittests/data_structures/segment_tree/test_segment_tree.cpp
+++ unittests/data_structures/segment_tree/test_segment_tree.cpp
@@ -2,16 +2,28 @@
 
 #include "data_structures/segment_tree/segment_tree.hpp"
 
-struct IntSum {
-  int operator()(const int& left, const int& right) const noexcept {
+template <typename T>
+struct Sum {
+  T operator()(const T& left, const T& right) const noexcept {
     return left + right;
   }
 };
 
-TEST(SegmentTree, SimpleTest) {
-  std::vector<int> vec = {1, 2, 3};
-  ads::SegmentTree<int, IntSum, 0> segment_tree(vec);
+// TODO: add more tests
+TEST(SegmentTree, CreateTree) {
+  std::vector<int> vec = {1, 2, 3, 7, 10};
+  ads::SegmentTree<int, Sum<int>, 0> segment_tree(vec);
   EXPECT_EQ(3, segment_tree.segmentQuery(0ULL, 1ULL));
+  EXPECT_EQ(20, segment_tree.segmentQuery(2ULL, 4ULL));
+}
+
+TEST(SegmentTree, UpdateTree) {
+  std::vector<int> vec = {1, 2, 3, 7, 10};
+  ads::SegmentTree<int, Sum<int>, 0> segment_tree(vec);
+  segment_tree.indexUpdate(0, 10);
+  segment_tree.indexUpdate(1, 7);
+  EXPECT_EQ(20, segment_tree.segmentQuery(0ULL, 2ULL));
+  EXPECT_EQ(20, segment_tree.segmentQuery(2ULL, 4ULL));
 }
 
 int main(int argc, char* argv[]) {
