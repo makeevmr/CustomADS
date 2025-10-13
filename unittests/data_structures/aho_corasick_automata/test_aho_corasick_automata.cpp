30236c131b4a9bb76018ce8024d4d0d8c0ec22ac
makeevmr <art.makeev2016@yandex.ru>
2025-10-13 20:15:11 +0300

    test: add tests for aho corasick algo

--- unittests/data_structures/aho_corasick_automata/test_aho_corasick_automata.cpp
+++ unittests/data_structures/aho_corasick_automata/test_aho_corasick_automata.cpp
@@ -1,26 +1,43 @@
+#include <unordered_map>
+#include <unordered_set>
+
 #include <gtest/gtest.h>
 
 #include "data_structures/aho_corasick_automata/aho_corasick_automata.hpp"
 
 typedef ads::AhoCorasickAutomata<'a', 'z'> LetterAhoCorasickAutomata;
 
+void expectSetEquality(
+    const LetterAhoCorasickAutomata::occurrences& occurrences,
+    const std::unordered_map<std::size_t, std::unordered_set<std::size_t>>&
+        expected_occurrences) {
+  std::size_t expected_occurrences_total_size = 0;
+  for (const auto& [_, set] : expected_occurrences) {
+    expected_occurrences_total_size += set.size();
+  }
+  EXPECT_EQ(occurrences.size(), expected_occurrences_total_size);
+  for (const auto& occurrence : occurrences) {
+    auto f_iter = expected_occurrences.find(occurrence.str_start_pos_);
+    EXPECT_TRUE(f_iter != expected_occurrences.end());
+    EXPECT_TRUE(f_iter->second.contains(occurrence.str_num_));
+  }
+}
+
 TEST(AhoCorasickAutomata, SimpleTest) {
   LetterAhoCorasickAutomata automata;
   automata.addString("he");
   automata.addString("she");
   automata.addString("hers");
-  automata.addString("his");
   const std::string text = "ahishers";
-  LetterAhoCorasickAutomata::occurrences occurrences =
-      automata.findAllOccurrences(text);
-  LetterAhoCorasickAutomata::occurrences expected_occurrences = {
-      {3, 3}, {5, 1}, {5, 0}, {7, 2}};
-  EXPECT_EQ(occurrences.size(), expected_occurrences.size());
-  for (std::size_t i = 0; i < occurrences.size(); ++i) {
-    EXPECT_EQ(occurrences[i].str_end_pos_,
-              expected_occurrences[i].str_end_pos_);
-    EXPECT_EQ(occurrences[i].str_num_, expected_occurrences[i].str_num_);
-  }
+  std::unordered_map<std::size_t, std::unordered_set<std::size_t>>
+      expected_occurrences;
+  expected_occurrences[4].insert(0);
+  expected_occurrences[3].insert(1);
+  expected_occurrences[4].insert(2);
+  expectSetEquality(automata.findAllOccurrences(text), expected_occurrences);
+  automata.addString("his");
+  expected_occurrences[1].insert(3);
+  expectSetEquality(automata.findAllOccurrences(text), expected_occurrences);
 }
 
 int main(int argc, char* argv[]) {
