e9521389b282131e30e597f00703fe0ec298566a
makeevmr <art.makeev2016@yandex.ru>
2025-10-14 22:14:26 +0300

    refactor: queue ds refactored

--- src/data_structures/queue/queue.hpp
+++ src/data_structures/queue/queue.hpp
@@ -10,16 +10,10 @@ namespace ads {
 // If N > 0 then allocate memory on stack
 template <typename T, std::size_t N>
 class Queue {
-private:
-  T data_[N];
-  std::size_t size_;
-  std::size_t top_pointer_;
-  std::size_t back_pointer_;
-
+public:
   using reference = T&;
   using const_reference = const T&;
 
-public:
   Queue() noexcept
       : size_(0),
         top_pointer_(0),
@@ -106,105 +100,21 @@ public:
       ++size_;
     }
   }
+
+private:
+  T data_[N];
+  std::size_t size_;
+  std::size_t top_pointer_;
+  std::size_t back_pointer_;
 };
 
 // If N == 0 then allocate memory on heap
 template <typename T>
 class Queue<T, 0> {
-private:
-  T* data_;
-  std::size_t size_;
-  std::size_t capacity_;
-  std::size_t top_pointer_;
-  std::size_t back_pointer_;
-
+public:
   using reference = T&;
   using const_reference = const T&;
 
-  void swap(Queue<T, 0>& other) noexcept {
-    std::swap(data_, other.data_);
-    std::swap(size_, other.size_);
-    std::swap(capacity_, other.capacity_);
-    std::swap(top_pointer_, other.top_pointer_);
-    std::swap(back_pointer_, other.back_pointer_);
-  }
-
-  void free(std::size_t destructor_calls) noexcept {
-    if (!std::is_trivially_destructible_v<T>) {
-      std::size_t destruction_index = top_pointer_;
-      std::size_t destroyed_objects = 0;
-      while (destroyed_objects < destructor_calls) {
-        (data_ + destruction_index)->~T();
-        ++destruction_index %= capacity_;
-        ++destroyed_objects;
-      }
-    }
-    ::operator delete(data_);
-  }
-
-  void uninitializedCopy(const Queue<T, 0>& other) {
-    std::size_t copied_objects = 0;
-    std::size_t other_data_index = other.top_pointer_;
-    try {
-      for (; copied_objects < other.size_; ++copied_objects) {
-        new (data_ + copied_objects) T(other.data_[other_data_index]);
-        ++other_data_index %= other.capacity_;
-      }
-    } catch (...) {
-      free(copied_objects);
-      throw;
-    }
-  }
-
-  void resize() {
-    if (capacity_ == 0) {
-      ::operator delete(data_);
-      capacity_ = 1;
-      data_ = reinterpret_cast<T*>(::operator new(sizeof(T) * capacity_));
-    } else {
-      std::size_t new_capacity = capacity_ * 2;
-      T* new_data =
-          reinterpret_cast<T*>(::operator new(sizeof(T) * new_capacity));
-      std::size_t copied_objects = 0;
-      std::size_t old_data_pointer = top_pointer_;
-      if (std::is_move_constructible_v<T>) {
-        while (copied_objects < size_) {
-          new (new_data + copied_objects) T(std::move(data_[old_data_pointer]));
-          ++old_data_pointer %= capacity_;
-          ++copied_objects;
-        }
-        ::operator delete(data_);
-      } else {
-        try {
-          while (copied_objects < size_) {
-            new (new_data + copied_objects) T(data_[old_data_pointer]);
-            ++old_data_pointer %= capacity_;
-            ++copied_objects;
-          }
-        } catch (...) {
-          if (!std::is_trivially_destructible_v<T>) {
-            for (std::size_t j = 0; j < copied_objects; ++j) {
-              (new_data + j)->~T();
-            }
-          }
-          ::operator delete(new_data);
-          throw;
-        }
-        if (!std::is_trivially_destructible_v<T>) {
-          for (std::size_t i = 0; i < size_; ++i) {
-            (data_ + i)->~T();
-          }
-        }
-        ::operator delete(data_);
-      }
-      data_ = new_data;
-      capacity_ = new_capacity;
-      top_pointer_ = 0;
-      back_pointer_ = size_;
-    }
-  }
-
-public:
   explicit Queue<T, 0>(std::size_t capacity)
       : data_(reinterpret_cast<T*>(::operator new(sizeof(T) * capacity))),
         size_(0),
@@ -347,6 +257,96 @@ public:
     ++back_pointer_ %= capacity_;
     ++size_;
   }
+
+private:
+  void swap(Queue<T, 0>& other) noexcept {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(top_pointer_, other.top_pointer_);
+    std::swap(back_pointer_, other.back_pointer_);
+  }
+
+  void free(std::size_t destructor_calls) noexcept {
+    if (!std::is_trivially_destructible_v<T>) {
+      std::size_t destruction_index = top_pointer_;
+      std::size_t destroyed_objects = 0;
+      while (destroyed_objects < destructor_calls) {
+        (data_ + destruction_index)->~T();
+        ++destruction_index %= capacity_;
+        ++destroyed_objects;
+      }
+    }
+    ::operator delete(data_);
+  }
+
+  void uninitializedCopy(const Queue<T, 0>& other) {
+    std::size_t copied_objects = 0;
+    std::size_t other_data_index = other.top_pointer_;
+    try {
+      for (; copied_objects < other.size_; ++copied_objects) {
+        new (data_ + copied_objects) T(other.data_[other_data_index]);
+        ++other_data_index %= other.capacity_;
+      }
+    } catch (...) {
+      free(copied_objects);
+      throw;
+    }
+  }
+
+  void resize() {
+    if (capacity_ == 0) {
+      ::operator delete(data_);
+      capacity_ = 1;
+      data_ = reinterpret_cast<T*>(::operator new(sizeof(T) * capacity_));
+    } else {
+      std::size_t new_capacity = capacity_ * 2;
+      T* new_data =
+          reinterpret_cast<T*>(::operator new(sizeof(T) * new_capacity));
+      std::size_t copied_objects = 0;
+      std::size_t old_data_pointer = top_pointer_;
+      if (std::is_move_constructible_v<T>) {
+        while (copied_objects < size_) {
+          new (new_data + copied_objects) T(std::move(data_[old_data_pointer]));
+          ++old_data_pointer %= capacity_;
+          ++copied_objects;
+        }
+        ::operator delete(data_);
+      } else {
+        try {
+          while (copied_objects < size_) {
+            new (new_data + copied_objects) T(data_[old_data_pointer]);
+            ++old_data_pointer %= capacity_;
+            ++copied_objects;
+          }
+        } catch (...) {
+          if (!std::is_trivially_destructible_v<T>) {
+            for (std::size_t j = 0; j < copied_objects; ++j) {
+              (new_data + j)->~T();
+            }
+          }
+          ::operator delete(new_data);
+          throw;
+        }
+        if (!std::is_trivially_destructible_v<T>) {
+          for (std::size_t i = 0; i < size_; ++i) {
+            (data_ + i)->~T();
+          }
+        }
+        ::operator delete(data_);
+      }
+      data_ = new_data;
+      capacity_ = new_capacity;
+      top_pointer_ = 0;
+      back_pointer_ = size_;
+    }
+  }
+
+  T* data_;
+  std::size_t size_;
+  std::size_t capacity_;
+  std::size_t top_pointer_;
+  std::size_t back_pointer_;
 };
 
 }  // namespace ads
